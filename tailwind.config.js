d063602fbabb3f1c4d3a937757c7d4e42dd2786e
PabitoVR <pabiszjakub@gmail.com>
2026-03-19 18:55:15 +0100

    Update Tailwind CSS configuration for colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.html"],
+  content: ["./index.html", "./*.html"],
   theme: {
     extend: {
       colors: {
-        primary: {
+        blue: {
           400: '#f472b6',
           500: '#ec4899',
           600: '#db2777',
@@ -12,12 +12,13 @@ module.exports = {
           800: '#9d174d',
           900: '#831843',
         },
-        ai: {
-          400: '#a78bfa',
-          500: '#8b5cf6',
-          600: '#7c3aed',
-          700: '#6d28d9',
-          800: '#5b21b6',
+        primary: {
+          400: '#f472b6',
+          500: '#ec4899',
+          600: '#db2777',
+          700: '#be185d',
+          800: '#9d174d',
+          900: '#831843',
         }
       },
     },
