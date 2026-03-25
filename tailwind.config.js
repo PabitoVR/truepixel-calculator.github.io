a2df9b822ca90aacccccf02b8c0d6cf5c0b7effb
pabiszjakubtc-lgtm <pabisz.jakub.tc@edu.zstjaslo.pl>
2026-03-25 14:44:19 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./*.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: {
-          50: '#fdf2f8', 
-          100: '#fce7f3',
-          200: '#fbcfe8',
-          300: '#f9a8d4',
-          400: '#f472b6',
-          500: '#ec4899', 
-          600: '#db2777',
-          700: '#be185d',
-          800: '#9d174d',
-          900: '#831843', 
-          950: '#500724',
-        }
-      },
-    },
-  },
-  plugins: [],
+  content: ["./index.html", "./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        pink: {
+          400: '#f9a8d4', 
+          500: '#f472b6', 
+          600: '#db2777',
+          700: '#be185d',
+          800: '#9d174d', 
+          900: '#500724', 
+        },
+       
+        primary: {
+          400: '#f9a8d4',
+          500: '#f472b6',
+          600: '#db2777',
+          700: '#be185d',
+          800: '#9d174d',
+          900: '#500724',
+        }
+      },
+    },
+  },
+  plugins: [],
 }
